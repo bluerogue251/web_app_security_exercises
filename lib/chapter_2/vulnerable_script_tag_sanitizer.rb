713336778be3a3e1b45016ff4c66594996195b8c
Teddy <theodore.widom@gmail.com>
2014-12-07 20:58:32 -0500

    Find suitable attack string

--- lib/chapter_2/vulnerable_script_tag_sanitizer.rb
+++ lib/chapter_2/vulnerable_script_tag_sanitizer.rb
@@ -3,11 +3,13 @@ require 'uri'
 module Chapter2
   class VulnerableScriptTagSanitizer
     def self.sanitize(input)
-      input.gsub!('<script>', '')
-      input.gsub!('</script>', '')
+      repeat = true if input.gsub!('<script>', '')
+      repeat = true if input.gsub!('</script>', '')
+      repeat = true if input.length > 50
       input = input[0..49]
-      input.gsub!('"', '')
-      URI.decode(input)
+      repeat = true if input.gsub!('"', '')
+      input = URI.decode(input)
+      repeat ? sanitize(input) : input
     end
   end
 end
--- spec/chapter_2/vulnerable_script_tag_sanitizer_spec.rb
+++ spec/chapter_2/vulnerable_script_tag_sanitizer_spec.rb
@@ -3,11 +3,11 @@ require 'chapter_2/vulnerable_script_tag_sanitizer'
 describe Chapter2::VulnerableScriptTagSanitizer do
   let(:sanitizer) { Chapter2::VulnerableScriptTagSanitizer }
 
-  it 'Strips any <script> tags that appear' do
+  it 'Strips <script> tags' do
     expect(sanitizer.sanitize('<script>')).to eq ''
   end
 
-  it 'Strips any </script> tags that appear' do
+  it 'Strips </script> tags' do
     expect(sanitizer.sanitize('</script>')).to eq ''
   end
 
@@ -15,7 +15,7 @@ describe Chapter2::VulnerableScriptTagSanitizer do
     expect(sanitizer.sanitize('a' * 51)).to eq('a' * 50)
   end
 
-  it 'Removes any quotation marks' do
+  it 'Removes quotation marks' do
     expect(sanitizer.sanitize('I am a "string')).to eq('I am a string')
   end
 
@@ -23,11 +23,16 @@ describe Chapter2::VulnerableScriptTagSanitizer do
     expect(sanitizer.sanitize('Hello%20G%C3%BCnter')).to eq('Hello Günter')
   end
 
-  it 'Repeats itself if any deletions were made' do
-    fail
+  it 'Removes multiple levels of script tags' do
+    expect(sanitizer.sanitize('<sc<script></script>ript></sc</script>ript>')).to eq('')
   end
 
-  it 'Is has vulnerabilities' do
-    expect(sanitizer.sanitize('malicious input')).to eq('“><script>alert(“foo”)</script>')
+  it 'Removes quotation marks after they are decoded when something else malicious was found' do
+    url_encoded_quotation_mark = URI.encode('"')
+    expect(sanitizer.sanitize('<script>' + url_encoded_quotation_mark)).to eq('')
+  end
+
+  it 'Still has vulnerabilities' do
+    expect(sanitizer.sanitize('%22>%3cscript>alert(%22foo%22)%3c/script>')).to eq('"><script>alert("foo")</script>')
   end
 end
